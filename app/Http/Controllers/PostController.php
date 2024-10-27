28cf7d02cca1b985ae9502a8761029d055c35e75
soramati <ishi.tokuri.asborake87@gmail.com>
2024-10-27 00:46:03 +0000

    editPageAdd

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -59,5 +59,19 @@ public function store(Post $post, PostRequest $request) // 引数をRequestか�
     
     return redirect('/posts/' . $post->id);
 }
+
+public function edit(Post $post)
+{
+    return view('posts.edit')->with(['post' => $post]);
+}
+
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
+}
+
 }
 ?>
